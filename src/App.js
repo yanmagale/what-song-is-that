26237fac66d8d0c6b9b971fa862e165ccc0e03d6
Yan Magalhães <yanmagale@gmail.com>
2020-02-12 17:58:43 -0300

    feat: update main title app

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ class App extends Component {
     super();
     this.state = {
       listOfTracks: ['Roots Bloody Roots', 'Living On a Prayer', 'You shook me all nigth long', 'Enter Sadman', 'Smells Like Teen Spirit',
-      'Jeremy', 'The Final Coutdown', 'I Wanna Rock', 'November Rain', 'The Trooper',
-      'Girls Girls Girls', 'Come as you are', 'The Zoo', 'Raining Blood',
-      'Send me an angel', 'Back in Black', 'What about love', 'Toxicity',
-      'Always', 'Its my life', 'Master of Puppets', 'Sad but true',
-      'Rock you like a hurrica', 'Holiday', 'Fear of the dark', 'I want out',
-      'Never forget, Never repeat', 'Machine Messiah', 'Dance of death',
-      'Brave new World', 'Stone', 'Blaze of Glory', 'Santa Fe', 'Faint', 'In The End',
-      'Wanted Dead or Alive', 'The Memory Remains', 'Man in the box', 'Hail to the king'],
+        'Jeremy', 'The Final Coutdown', 'I Wanna Rock', 'November Rain', 'The Trooper',
+        'Girls Girls Girls', 'Come as you are', 'The Zoo', 'Raining Blood',
+        'Send me an angel', 'Back in Black', 'What about love', 'Toxicity',
+        'Always', 'Its my life', 'Master of Puppets', 'Sad but true',
+        'Rock you like a hurrica', 'Holiday', 'Fear of the dark', 'I want out',
+        'Never forget, Never repeat', 'Machine Messiah', 'Dance of death',
+        'Brave new World', 'Stone', 'Blaze of Glory', 'Santa Fe', 'Faint', 'In The End',
+        'Wanted Dead or Alive', 'The Memory Remains', 'Man in the box', 'Hail to the king'],
       passedTracks: [],
       track: {},
       isShowTrack: false
@@ -21,18 +21,18 @@ class App extends Component {
   }
 
   render() {
-    const {track, isShowTrack} = this.state;
+    const { track, isShowTrack } = this.state;
     return (
       <div className="container">
-        <h2>What Song Is That ??</h2>
+        <h2>What Song Is This ??</h2>
         <button onClick={() => this.getSong()}>Get Music</button>
         {Object.keys(track).length > 0 && isShowTrack && (
           <React.Fragment>
             <div className="track">
-              This music is <span style={{fontStyle: 'italic'}}>{track.name}</span> from album
+              This music is <span style={{ fontStyle: 'italic' }}>{track.name}</span> from album
               <img src={track.album.images[1].url} />
-            </div>  
-          </React.Fragment> 
+            </div>
+          </React.Fragment>
         )}
       </div>
     );
@@ -40,46 +40,46 @@ class App extends Component {
   _generateRandomTrack() {
 
     const randomName = this.state.listOfTracks[Math.floor(Math.random() * this.state.listOfTracks.length)]
-    
+
     if (this.state.passedTracks.indexOf(randomName) > -1) {
       return this._generateRandomTrack();
     }
     let newPassedTracks = [...this.state.passedTracks, randomName]
-    this.setState({passedTracks: newPassedTracks})
+    this.setState({ passedTracks: newPassedTracks })
     return randomName;
   }
 
   async getSong() {
     const randomTrack = this._generateRandomTrack();
     const token = 'YOUR_TOKEN_HERE';
-    const data  = await fetch(`https://api.spotify.com/v1/search?q=${decodeURIComponent(randomTrack)}&type=track`,
-     {headers: {'Authorization': 'Bearer ' + token}})
+    const data = await fetch(`https://api.spotify.com/v1/search?q=${decodeURIComponent(randomTrack)}&type=track`,
+      { headers: { 'Authorization': 'Bearer ' + token } })
       .then(response => response.json())
       .catch(error => new Error('erro at get characters', error));
-    
+
     const trackId = data.tracks ? data.tracks.items[0].id : null;
 
     if (trackId) {
-      const track  = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`,
-     {headers: {'Authorization': 'Bearer ' + token}})
-      .then(response => response.json())
-      .catch(error => new Error('erro at get characters', error));
+      const track = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`,
+        { headers: { 'Authorization': 'Bearer ' + token } })
+        .then(response => response.json())
+        .catch(error => new Error('erro at get characters', error));
 
-      if(track.preview_url) {
+      if (track.preview_url) {
         let player = new Audio(track.preview_url);
         player.play();
-        this.setState({track: track})
+        this.setState({ track: track })
         setTimeout(() => {
-          this.setState({isShowTrack: true})
+          this.setState({ isShowTrack: true })
         }, 4000)
         setTimeout(() => {
           player.pause();
           player.currentTime = 0;
-          this.setState({isShowTrack: false})
+          this.setState({ isShowTrack: false })
         }, 10000)
       }
+    }
   }
-}
 }
 
 export default App;
